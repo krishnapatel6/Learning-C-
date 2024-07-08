5b0dfe1aaaa13e20754e5511daaabfbf199b1d5a
krishnapatel6 <krishnapatel101106@gmail.com>
2024-07-08 21:00:10 +0530

    new file added

---
+++ new.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    for(int i=0;i<=100;i++){
+        printf ("git add .\n");
+        printf ("git commit -m 'done'");
+        printf ("git push -u origin main");
+    }
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,14 +1,5 @@
-#include<stdio.h>
-void display(){
-    printf("Good Afternoon \n");
-}
 
-int main(){
-
-    int a;
-    printf('%d',a);
-    printf("Good morning \n");
-    display();
-    printf("Good night \n");
-    return 0;
-}
+    int number,guess,nguesses=1;
+    srand(time(0));
+    number = rand()%100+1;//Generates random number 1 to 100
+    //printf("The number
